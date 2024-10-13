84bfd5c4b7d34ba77fde25951114624c0afc927f
ncc02 <cuongtk2002@gmail.com>
2024-10-13 13:50:52 +0700

    API hoi dap

--- knowledgeGraph/app/urls.py
+++ knowledgeGraph/app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .api_views.ApiFolderView import *
 from .api_views.ApiFileView import *
+from .views import chatbot
 
 urlpatterns = [
     path('folder/', FolderListView.as_view(), name='folder-list'),            
@@ -12,4 +13,6 @@ urlpatterns = [
     path('file/add/', FileCreateView.as_view(), name='file-add'),
     path('file/update/', FileUpdateView.as_view(), name='file-update'),
     path('file/delete/',FileDeleteView.as_view(), name='file-delete'),
+    
+    path('chatbot/', chatbot)
 ]
