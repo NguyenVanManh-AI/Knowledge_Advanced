409ec9506fe301018641b720552c1fbac50882fa
Nguyễn Văn Mạnh <81563451+NguyenVanManh-AI@users.noreply.github.com>
2024-10-25 01:47:38 +0700

    fe : config

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,7 @@
 export default {
-	// API_URL: 'https://playerbillionmoney.pythonanywhere.com/api/',
-	// URL: 'https://playerbillionmoney.pythonanywhere.com/',
-	API_URL: 'http://localhost:8000/api/',
-	URL: 'http://localhost:8000',
+	API_URL: 'https://playerbillionmoney.pythonanywhere.com/api/',
+	URL: 'https://playerbillionmoney.pythonanywhere.com',
+	// API_URL: 'http://localhost:8000/api/',
+	// URL: 'http://localhost:8000',
 	DOMAIN_CLIENT: 'http://localhost:8080',
 }
